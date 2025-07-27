97ba3e30c515bb2aa9e2c0e7eec5020a2d7e47fb
Sajib Ahmed <112968850+mollah2022@users.noreply.github.com>
2025-07-27 21:00:22 +0600

    Add files via upload

---
+++ initialTask.js
@@ -0,0 +1,33 @@
+const initialTask = [
+  {
+    id: 1,
+    text: "Learn HTML",
+    done: true,
+  },
+  {
+    id: 2,
+    text: "Learn CSS",
+    done: false,
+  },
+  {
+    id: 3,
+    text: "Learn JavaScript",
+    done: true,
+  },
+  {
+    id: 4,
+    text: "Learn React",
+    done: false,
+  },
+  {
+    id: 5,
+    text: "Learn Next.js",
+    done: true,
+  },
+  {
+    id: 6,
+    text: "Learn Mongodb",
+    done: false,
+  },
+];
+export default initialTask;
