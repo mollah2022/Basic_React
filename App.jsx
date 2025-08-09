fc7bf7024d6f0869754cbad55dd7aceea8ad7cdb
Sajib Ahmed <112968850+mollah2022@users.noreply.github.com>
2025-08-09 21:15:19 +0600

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,54 +1,22 @@
-import { useState } from "react";
-import AddTask from "./component/AddTask";
-import TaskList from "./component/TaskList";
-import initialTask from "./data/initialTask";
-
-let indexId = 7;
+import { Routes, Route } from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
+import Home from "./pages/Home";
+import Jobs from "./pages/Jobs";
+import JobDetails from "./pages/JobDetails";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
 function App() {
-  const [tasks, setTask] = useState(initialTask);
-
-  function handleChange(task) {
-    const chnageTask = tasks.map((t) => {
-      if (t.id === task.id) {
-        return {
-          ...t,
-          text: task.text,
-          done: task.done,
-        };
-      } else {
-        return t;
-      }
-    });
-    setTask(chnageTask);
-  }
-
-  function deleteTask(taskId) {
-    const deleteValue = tasks.filter((task) => {
-      if (task.id !== taskId) {
-        return { ...task };
-      }
-    });
-    setTask(deleteValue);
-  }
-
-  function handleAddTask(text) {
-    setTask([
-      ...tasks,
-      {
-        id: indexId++,
-        text: text,
-        done: false,
-      },
-    ]);
-  }
-
   return (
-    <>
-      <h1>Todo APP</h1>
-      <AddTask onAddTask={handleAddTask} />
-      <TaskList tasks={tasks} onchange={handleChange} ondelete={deleteTask} />
-    </>
+    <MainLayout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/jobs/:id" element={<JobDetails />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </MainLayout>
   );
 }
 
